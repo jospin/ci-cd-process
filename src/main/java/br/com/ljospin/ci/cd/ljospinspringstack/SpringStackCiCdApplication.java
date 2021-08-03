473a4888ec45cb1974d18263913f34be186732ff
lucienj <CrisLu@0912>
2021-08-03 09:07:25 -0300

    Config travis and git

---
+++ src/main/java/br/com/ljospin/ci/cd/ljospinspringstack/SpringStackCiCdApplication.java
@@ -0,0 +1,13 @@
+package br.com.ljospin.ci.cd.ljospinspringstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStackCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStackCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ljospin/ci/cd/ljospinspringstack/SpringStackCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ljospin.ci.cd.ljospinspringstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStackCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
